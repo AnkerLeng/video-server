52133ddfda178cb823e2ce4c3bae4b16f33edb80
anker <gyyx084@gmail.com>
2019-01-19 12:52:56 +0800

    实现User test

--- src/api/dbops/conn.go
+++ src/api/dbops/conn.go
@@ -11,7 +11,7 @@ var (
 )
 
 func init() {
-	dbConn, err = sql.Open("mysql", "root:123!@#@tcp(localhost:3306)/video_server?charset=utf8")
+	dbConn, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/video_server?charset=utf8")
 	if err != nil {
 		panic(err.Error())
 	}
